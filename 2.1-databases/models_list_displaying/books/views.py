7c47bcf5b2ac16ac8ef75ace9bbda5a67fe9ae8c
Danil_Dobr <danil.bore@mail.ru>
2025-06-12 12:44:28 +0500

    доп задание +

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-
+from .models import Book
+from django.db.models import Q
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.order_by('pub_date')
+    context = {'books': books}
+    return render(request, template, context)
+
+def books_by_date(request, pub_date):
+    template = 'books/books_by_date.html'
+    books = Book.objects.filter(pub_date=pub_date)
+    
+    # Получаем предыдущую и следующую даты
+    prev_date = Book.objects.filter(
+        pub_date__lt=pub_date
+    ).order_by('-pub_date').values('pub_date').first()
+    
+    next_date = Book.objects.filter(
+        pub_date__gt=pub_date
+    ).order_by('pub_date').values('pub_date').first()
+    
+    context = {
+        'books': books,
+        'prev_date': prev_date['pub_date'] if prev_date else None,
+        'next_date': next_date['pub_date'] if next_date else None,
+        'current_date': pub_date,
+    }
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_by_date, books_view
 
 urlpatterns = [
     path('', books_view, name='books'),
+    path('books/<str:pub_date>/', books_by_date, name='books_by_date'),
     path('admin/', admin.site.urls),
 ]
