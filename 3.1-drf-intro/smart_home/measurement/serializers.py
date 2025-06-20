dec244809236de4581d1f8641e5c85ef407b2dc5
Danil_Dobr <danil.bore@mail.ru>
2025-06-20 21:52:43 +0500

    выполненное зажанин 3.1

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,23 @@
 from rest_framework import serializers
+from .models import Sensor, Measurement
 
-# TODO: опишите необходимые сериализаторы
+
+class SensorSerializer(serializers.ModelSerializer):
+    # Для списка датчиков (GET /sensors/)
+     # Для создания датчика (POST /sensors/)
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+        
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = ['sensor', 'temperature', 'created_at']
+
+class SensorDetailSerializer(serializers.ModelSerializer):
+    measurements = MeasurementSerializer(read_only=True, many=True)
+    # read_only=True - поле только для чтения (не требует ввода при создании/обновлении)
+    # many=True - указывает, что это отношение "один-ко-многим" (у датчика много измерений)
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', views.SensorListCreateView.as_view()),  # GET список, POST создание
+    path('sensors/<pk>/', views.SensorRetrieveUpdateView.as_view()),  # GET детали, PATCH обновление
+    path('measurements/', views.MeasurementCreateView.as_view()),  # POST добавление измерения
 ]
